0efb9de56246a64ab32b88b4bbdf106f028fdecd
Furkan <info@furkanbilgin.net>
2024-10-27 15:01:39 +0300

    feat: add security views

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_admin import Admin
 from flask_alembic import Alembic
 from flask_babel import Babel
+from flask_bootstrap import Bootstrap5
 from flask_security import FSQLALiteUserDatastore, Security
 
 from database import Model, db
@@ -13,13 +14,19 @@ user_datastore = FSQLALiteUserDatastore(db, User, Role)
 admin = Admin()
 security = Security()
 babel = Babel()
+bootstrap = Bootstrap5()
 
 
 def create_app():
     app = Flask(__name__)
     with app.app_context():
+        from views import get_blueprints
+
+        for blueprint in get_blueprints():
+            app.register_blueprint(blueprint)
+
         app.config.from_pyfile("settings.py")
-        for plugin in [admin, db, alembic, babel]:
+        for plugin in [admin, db, alembic, babel, bootstrap]:
             plugin.init_app(app)
         security.init_app(app, user_datastore)
 
---
+++ src/views/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+import os
+import pkgutil
+
+
+def get_blueprints():
+    blueprints = []
+    current_dir = os.path.dirname(__file__)
+    for _, module_name, _ in pkgutil.iter_modules([current_dir]):
+        module = importlib.import_module(f".{module_name}", package="views")
+        if hasattr(module, "blueprint"):
+            blueprints.append(module.blueprint)
+
+    return blueprints
