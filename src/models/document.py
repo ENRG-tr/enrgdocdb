1b74aed64ce20d3938bbae90849adf9a8558cba0
Furkan <info@furkanbilgin.net>
2024-10-28 00:25:27 +0300

    feat: fix author and topic display in document view

--- src/models/document.py
+++ src/models/document.py
@@ -2,7 +2,9 @@ from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Model
+from models.author import Author
 from models.base import Base
+from models.topic import Topic
 
 
 class Document(Base, Model):
@@ -28,12 +30,16 @@ class DocumentAuthor(Base, Model):
     document_id: Mapped[int] = mapped_column(Integer, ForeignKey("documents.id"))
     author_id: Mapped[int] = mapped_column(Integer, ForeignKey("authors.id"))
 
+    author: Mapped[Author] = relationship("Author")
+
 
 class DocumentTopic(Base, Model):
     __tablename__ = "document_topics"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     document_id: Mapped[int] = mapped_column(Integer, ForeignKey("documents.id"))
     topic_id: Mapped[int] = mapped_column(Integer, ForeignKey("topics.id"))
+
+    topic: Mapped[Topic] = relationship("Topic")
 
 
 class DocumentType(Base, Model):
