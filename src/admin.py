d19273dc470de83e16b510b50ed9c15651263e6a
Furkan Bilgin <info@furkanbilgin.net>
2025-01-04 21:31:51 +0300

    fix: fix

--- src/admin.py
+++ src/admin.py
@@ -109,14 +109,7 @@ class UserAdminView(AdminView):
 
 
 class EventAdminView(AdminView):
-    form_columns = [
-        "title",
-        "location",
-        "event_url",
-        "session_time",
-        "topics",
-        "moderators",
-    ]
+    form_columns = ["title", "location", "event_url", "date", "topics", "moderators"]
 
     inline_models = [
         (
