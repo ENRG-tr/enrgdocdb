2c8e008594e10e35d90094870c113925ca6d1fee
Furkan Bilgin <info@furkanbilgin.net>
2025-01-05 13:23:03 +0300

    feat: add iCalendar export functionality to event calendar

--- src/views/event.py
+++ src/views/event.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, abort, render_template
+import jwt
+import pytz
+from flask import Blueprint, Response, abort, render_template, url_for
+from flask import current_app as app
+from flask_login import current_user
+from icalendar import Calendar
+from icalendar import Event as ICalEvent
 
 from database import db
 from models.event import Event
-from models.user import RolePermission
+from models.user import RolePermission, User
 from utils.security import permission_check, secure_blueprint
 
 blueprint = Blueprint("event", __name__, url_prefix="/events")
@@ -23,4 +29,37 @@ def view(event_id: int):
 @blueprint.route("/calendar")
 def calendar():
     events = db.session.query(Event).order_by(Event.date.desc()).limit(300).all()
-    return render_template("docdb/event_calendar.html", events=events)
+    jwt_token = jwt.encode({"user_id": current_user.id}, app.config["SECRET_KEY"])
+    icalendar_url = url_for("icalendar_all", jwt_token=jwt_token, _external=True)
+    return render_template(
+        "docdb/event_calendar.html", events=events, icalendar_url=icalendar_url
+    )
+
+
+@app.route("/events/icalendar/all/<string:jwt_token>")
+def icalendar_all(jwt_token: str):
+    try:
+        decoded = jwt.decode(jwt_token, app.config["SECRET_KEY"], algorithms=["HS256"])
+    except Exception as e:
+        return abort(403)
+
+    user = decoded["user_id"]
+    user: User = db.session.query(User).get(user)
+    if not user or not user.is_active:
+        return abort(403)
+
+    events = db.session.query(Event).all()
+    cal = Calendar()
+
+    for event in events:
+        ical_event = ICalEvent()
+        ical_event.add("summary", event.title)
+        utc_date = event.date.replace(tzinfo=pytz.UTC)
+        ical_event.add("dtstart", utc_date)
+        ical_event.add("description", event.location + "\n" + event.event_url)
+        ical_event.add("location", event.location)
+        cal.add_component(ical_event)
+
+    response = Response(cal.to_ical(), mimetype="text/calendar")
+    response.headers["Content-Disposition"] = "attachment; filename=all_events.ics"
+    return response
