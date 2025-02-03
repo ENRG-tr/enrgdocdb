9b504961bfa59544dc725e7c0379b30317f51655
Furkan Bilgin <info@furkanbilgin.net>
2025-02-03 20:13:22 +0300

    refactor: update slack auth to use openid connect

--- src/oauth/slack.py
+++ src/oauth/slack.py
@@ -27,7 +27,9 @@ def normalize_userinfo(client, data):
     return params
 
 
-def _monkeypatch_user_datastore_for_oauth(user_datastore: UserDatastore, email: str):
+def _monkeypatch_user_datastore_for_oauth(
+    user_datastore: UserDatastore, email: str, first_name: str, last_name: str
+):
     org_find_user = user_datastore.find_user
 
     def restore_datastore(response=None):
@@ -40,6 +42,8 @@ def _monkeypatch_user_datastore_for_oauth(user_datastore: UserDatastore, email: 
             user = user_datastore.create_user(
                 email=email, password=hash_password(token_urlsafe().encode())
             )
+            user.first_name = first_name
+            user.last_name = last_name
         return user
 
     user_datastore.find_user = find_user
@@ -52,20 +56,27 @@ class SlackFsOauthProvider(FsOAuthProvider):
         return {
             "client_id": environ.get("FLASK_SLACK_CLIENT_ID"),
             "api_base_url": "https://slack.com/api/",
-            "access_token_url": "https://slack.com/api/oauth.access",
-            "authorize_url": "https://slack.com/oauth/authorize",
+            "access_token_url": "https://slack.com/api/openid.connect.token",
+            "authorize_url": "https://slack.com/openid/connect/authorize",
             "client_kwargs": {
                 "token_endpoint_auth_method": "client_secret_post",
-                "scope": "identity.basic identity.avatar identity.email",
+                "scope": "profile openid email",
             },
-            "userinfo_endpoint": "users.identity",
+            "userinfo_endpoint": "openid.connect.userInfo",
             "userinfo_compliance_fix": normalize_userinfo,
+            "server_metadata_url": "https://slack.com/.well-known/openid-configuration",
         }
 
     def fetch_identity_cb(self, oauth, token):  # pragma no cover
         from app import user_datastore
 
-        profile = token["user"]
-        email = profile["email"]
-        after_this_request(_monkeypatch_user_datastore_for_oauth(user_datastore, email))
+        profile = token["userinfo"]
+        after_this_request(
+            _monkeypatch_user_datastore_for_oauth(
+                user_datastore,
+                profile["email"],
+                profile["given_name"],
+                profile["family_name"],
+            )
+        )
         return "email", profile["email"]
