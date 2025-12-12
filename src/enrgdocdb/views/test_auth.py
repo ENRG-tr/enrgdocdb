62290ac5cf7d23b93d5aa23c72109fc4166227c4
Furkan Bilgin <info@furkanbilgin.net>
2025-12-12 12:32:10 +0300

    feat: add has_role route for role-based access control

--- src/enrgdocdb/views/test_auth.py
+++ src/enrgdocdb/views/test_auth.py
@@ -9,3 +9,11 @@ def index():
     if current_user.is_authenticated:
         return "You are logged in as '{}'".format(current_user.email)
     return "You are not logged in", 401
+
+
+@blueprint.route("/has-role/<role>")
+def has_role(role):
+    if current_user.is_authenticated:
+        if any(role in r.name for r in current_user.roles):
+            return "You have the role '{}'".format(role)
+    return "You do not have the role '{}'".format(role), 401
